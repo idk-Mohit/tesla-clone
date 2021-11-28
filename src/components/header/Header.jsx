cef0d2f60d39867367a0d8b8a82ceb8b89468b96
Mohit <mototurbo12499@gmail.com>
2021-11-28 13:55:49 +0530

    doNE

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,34 +2,19 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from "@material-ui/icons/Close";
-import { selectCars } from '../../features/car/carSlice'
-import { useSelector } from 'react-redux'
 
 const Header = () => {
   const [burgerStatus, setBurgerStatus] = useState(false);
-  const cars = useSelector(selectCars);
-  console.log(cars)
   return (
     <Container>
         <a>
           <img src="/images/logo.svg" alt="Logo" />
         </a>
       <Menu>
-        {cars && cars.map((car,index)=>(
-          <a href="#">{car}</a>
-        ))}
-        <p>
           <a href="#">Model S</a>
-        </p>
-        <p>
           <a href="#">Model 3</a>
-        </p>
-        <p>
           <a href="#">Model X</a>
-        </p>
-        <p>
           <a href="#">Model Y</a>
-        </p>
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
